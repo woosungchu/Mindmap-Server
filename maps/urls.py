a6e56a22b18039a9516b39958b96e5558a540d36
woosungchu <woosungchu@gmail.com>
2017-02-27 16:02:42 +0900

    clean up test code

--- maps/urls.py
+++ maps/urls.py
@@ -8,10 +8,7 @@ router.register(r'maps', views.MapViewSet)
 router.register(r'users', views.UserViewSet)
 
 urlpatterns=[
-    url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^api/', include(router.urls, namespace='api')),
     url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^api-token-refresh/', refresh_jwt_token),
-    #url(r'^api-token-auth/', views.TestView.as_view()),
-    #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- maps/views.py
+++ maps/views.py
@@ -1,29 +1,7 @@
-from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import viewsets, permissions
-from rest_framework.decorators import api_view
-from rest_framework.renderers import TemplateHTMLRenderer
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.reverse import reverse
 from maps.models import Map
 from maps.serializers import MapSerializer, UserSerializer
-
-"""
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'maps': reverse('map-list', request=request, format=format)
-    })
-"""
-
-class IndexView(APIView):
-    renderer_classes = [TemplateHTMLRenderer]
-    template_name = "index.html"
-
-    def get(self, request):
-        return Response()
 
 class MapViewSet(viewsets.ModelViewSet):
     queryset = Map.objects.all()
@@ -38,18 +16,3 @@ class UserViewSet(viewsets.ModelViewSet):
 
     def perform_create(self,serializer):
         User.objects.create_user(**serializer.validated_data)
-
-""" Code for test
-from rest_framework.views import APIView
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import AllowAny
-
-class TestView(APIView):
-    authentication_classes = (TokenAuthentication, )
-    permission_classes = (AllowAny,)
-
-    def post(self, request, *args, **kwargs):
-        print(request.data);
-
-        return Response('test', status=status.HTTP_400_BAD_REQUEST)
-"""
