899f4e63f99f46d2bcf29103a25efdfa5bd7dff5
woosungchu <woosungchu@gmail.com>
2017-02-27 18:31:46 +0900

    add test.py

--- mindmap/maps/tests.py
+++ mindmap/maps/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from maps.models import Map
 
-# Create your tests here.
+class MapsTests(APITestCase):
+    def test_maps_list(self):
+        """
+        Ensure we can get list of map
+        """
+        url = reverse('map-list')
+        #data = {'name': 'DabApps'}
+        response = self.client.get(url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        #self.assertEqual(Account.objects.count(), 1)
+        #self.assertEqual(Account.objects.get().name, 'DabApps')
